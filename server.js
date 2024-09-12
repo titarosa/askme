5e5898bdc38725c83a2b34c601656e8ea50ca76e
Wesley Antico <Wesley@Wesleys-MacBook-Air.local>
2024-09-12 15:56:23 -0400

    updated database connection

--- server.js
+++ server.js
@@ -35,5 +35,6 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
 });
-
